6518d90760fe61fbd6db8bdf1e27c840213a1aff
Philip Hanson <grayman3@gmail.com>
2011-03-29 22:26:07 -0400

    Completed functionality to grab from database

--- ChurchView2EasyWorship/Program.cs
+++ ChurchView2EasyWorship/Program.cs
@@ -2,13 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Configuration;
+using System.Data.Common;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace ChurchView2EasyWorship
 {
     class Program
     {
+        static Dictionary<string, SongParts> x = new Dictionary<string, SongParts> {
+            { "Song", SongParts.Chorus1 },
+            { "Chorus2", SongParts.Chorus2 },
+            { "Chorus3", SongParts.Chorus3 },
+            { "Chorus4", SongParts.Chorus4 },
+            { "Verse1", SongParts.Verse1 },
+            { "Verse2", SongParts.Verse2 },
+            { "Verse3", SongParts.Verse3 },
+            { "Verse4", SongParts.Verse4 }
+        };
+
         static void Main(string[] args)
         {
+            // set up variables with default values
+            string dbName = ConfigurationManager.AppSettings["DefaultDbName"];
+            string folderName = ConfigurationManager.AppSettings["DefaultFolderName"];
+
+            // prepare for songs
+            List<Song> songs = new List<Song>();
+
+            // get argument list
+            if (args.Length > 0)
+                dbName = args[0];
+            if (args.Length > 1)
+                folderName = args[1];
+
+            // build the connection string
+            DbConnectionStringBuilder builder = new DbConnectionStringBuilder();
+            builder.ConnectionString = ConfigurationManager.ConnectionStrings["Access"].ConnectionString;
+            builder.Add("Data Source", dbName);
+
+            // get a new connection
+            using (IDbConnection conn = new System.Data.OleDb.OleDbConnection(builder.ConnectionString))
+            {
+                conn.Open();
+
+                IDbCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT SongTitle, Song, Chorus2, Chorus3, Chorus4, Verse1, Verse2, Verse3, Verse4 FROM Songs";
+                
+                using(IDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    Song currSong;
+                    while (reader.Read())
+                    {
+                        currSong = new Song(reader.GetString(0));
+
+                        for (int i = 1; i < reader.FieldCount; i++)
+                            if (!reader.IsDBNull(i))
+                                currSong.Parts.Add(x[reader.GetName(i)], reader.GetString(i));
+
+                        songs.Add(currSong);
+                    }
+                }
+            }
+
+            foreach (var song in songs)
+                song.Serialize(File.OpenWrite(Path.Combine(folderName, MakeValidFileName(song.Name))));
+        }
+
+        private static string MakeValidFileName(string name)
+        {
+            string invalidChars = Regex.Escape(new string(Path.GetInvalidFileNameChars()));
+            string invalidReStr = string.Format(@"[{0}]+", invalidChars);
+            return Regex.Replace(name, invalidReStr, "_");
         }
     }
 }
---
+++ ChurchView2EasyWorship/Song.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ChurchView2EasyWorship
+{
+    /// <summary>
+    /// Defines a single song.
+    /// Associates a song name with its constituent parts.
+    /// </summary>
+    public class Song
+    {
+        public string Name { get; set; }
+        public Dictionary<SongParts, string> Parts { get; set; }
+
+        /// <summary>
+        /// Instantiates a new <see cref="Song"/>.
+        /// </summary>
+        /// <param name="name">The song name.</param>
+        public Song(string name)
+        {
+            Name = name;
+            Parts = new Dictionary<SongParts, string>();
+        }
+
+        /// <summary>
+        /// Returns a string representation of the song.
+        /// </summary>
+        public override string ToString()
+        {
+            return string.Format("<Song(\"{0}\")>", Name);
+        }
+
+        /// <summary>
+        /// Outputs a text representation of the song to the given <see cref="System.IO.Stream"/>
+        /// </summary>
+        /// <param name="s">The stream.</param>
+        public void Serialize(Stream s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// List of song parts.
+    /// </summary>
+    public enum SongParts
+    {
+        Chorus1,
+        Chorus2,
+        Chorus3,
+        Chorus4,
+        Verse1,
+        Verse2,
+        Verse3,
+        Verse4
+    }
+}
